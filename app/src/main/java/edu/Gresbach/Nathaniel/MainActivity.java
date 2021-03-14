480dc66fade15169e7e7b82226f2ee041b5d284b
ngresbach <gresbac7@uwm.edu>
2021-03-13 20:22:02 -0600

    First Commit

---
+++ app/src/main/java/edu/Gresbach/Nathaniel/MainActivity.java
@@ -0,0 +1,43 @@
+package edu.Gresbach.Nathaniel;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Declare our variables
+    double costPerCredit;
+    int numberOfCredits;
+    double totalCostSemester;
+    String semesterSelected;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText costPerCredText=(EditText)findViewById(R.id.costPerCredit);
+        final EditText numOfCredText=(EditText)findViewById(R.id.numberOfCredits);
+        final Spinner group = (Spinner)findViewById(R.id.spinner);
+        final TextView resultTextBox=(TextView) findViewById(R.id.textView5);
+        Button calculate = (Button) findViewById(R.id.button);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                costPerCredit = Double.parseDouble(costPerCredText.getText( ).toString( ));
+                numberOfCredits = Integer.parseInt(numOfCredText.getText( ).toString( ));
+                semesterSelected = group.getSelectedItem( ).toString( );
+                totalCostSemester = numberOfCredits * costPerCredit;
+                DecimalFormat currency = new DecimalFormat("$###,###.##");
+
+                String resultText = "Your total cost for the " + semesterSelected +
+                        " semester is " + currency.format(totalCostSemester);
+                resultTextBox.setText(resultText);
+            }
+        });
+    }
+}
